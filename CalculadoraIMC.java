46c494b0fb9be62cb65c69a76fa6c85261f61d45
Ronan Soares <soaresronansilva@gmail.com>
2025-02-02 20:44:00 -0300

    Atualizar o CalculadoraIMC.java

--- CalculadoraIMC.java
+++ CalculadoraIMC.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class CalculadoraIMC {
 
     public static void main(String[] args) {
-
+        // habilita entrada de dados do teclado pelo metodo Scanner vc
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
         Pessoa pessoa = new Pessoa();
